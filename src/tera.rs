17ba7f238947a4080b42593c7ea40becb8aea941
Joshua Goins <josh@redstrate.com>
2024-04-16 22:07:50 -0400

    Add invalid data test for Terrain

--- src/tera.rs
+++ src/tera.rs
@@ -65,3 +65,21 @@ impl Terrain {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::read;
+    use std::path::PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn test_invalid() {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("resources/tests");
+        d.push("random");
+
+        // Feeding it invalid data should not panic
+        Terrain::from_existing(&read(d).unwrap());
+    }
+}
