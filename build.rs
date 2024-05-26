6700d1f3ace209c798e6502b3d0440042b8aa158
Joshua Goins <josh@redstrate.com>
2024-05-26 08:59:53 -0400

    Try to yeet that build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-fn main() {
-    #[cfg(feature = "game_install")]
-    println!("cargo::rustc-link-lib=unshield");
-}
