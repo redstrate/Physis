c90a46186f0cc54db930e2754db73bfc4cce36ad
Joshua Goins <josh@redstrate.com>
2024-06-29 09:55:08 -0400

    Don't probe system dependencies on Windows
    This should help unbreak the Astra build on Windows.

--- build.rs
+++ build.rs
@@ -2,5 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 fn main() {
+    // Windows doesn't ship pkgconfig files, typically. At least in our build system.
+    #[cfg(not(target_os = "windows"))]
     system_deps::Config::new().probe().unwrap();
 }
