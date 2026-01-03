1d739873e0ea21ace998f9a07e15353d07e1302c
Joshua Goins <josh@redstrate.com>
2026-01-03 17:12:24 -0500

    Expose more tweakable properties of Terrain
    I'm not quite certain of the new API, but this was needed anyway.

--- src/tera.rs
+++ src/tera.rs
@@ -14,59 +14,38 @@ use binrw::binrw;
 
 #[binrw]
 #[derive(Debug, Clone, Copy)]
-struct PlatePosition {
-    x: i16,
-    y: i16,
-}
-
-#[binrw]
-#[derive(Debug)]
-struct TerrainHeader {
-    // Example: 0x1000003
-    version: u32,
-    plate_count: u32,
-    plate_size: u32,
-    clip_distance: f32,
-
-    unknown: f32,
-
-    #[brw(pad_before = 32)]
-    #[br(count = plate_count)]
-    positions: Vec<PlatePosition>,
-}
-
-#[derive(Debug)]
-pub struct PlateModel {
-    pub position: (f32, f32),
-    pub filename: String,
+pub struct Plate {
+    pub x: i16,
+    pub y: i16,
 }
 
 /// Terrain file, usually with the `.tera` file extension.
 ///
 /// Contains a list of plates that make up the underlying terrain of a level.
-#[derive(Debug)]
+#[binrw]
+#[derive(Debug, Clone)]
 pub struct Terrain {
-    pub plates: Vec<PlateModel>,
+    // Example: 0x1000003
+    version: u32,
+
+    #[bw(calc = plates.len() as u32)]
+    plate_count: u32,
+
+    /// Size of each plate in units.
+    pub plate_size: u32,
+    pub clip_distance: f32,
+
+    unknown: f32,
+
+    #[brw(pad_before = 32)]
+    #[br(count = plate_count)]
+    pub plates: Vec<Plate>,
 }
 
 impl ReadableFile for Terrain {
     fn from_existing(platform: Platform, buffer: ByteSpan) -> Option<Terrain> {
         let mut cursor = Cursor::new(buffer);
-        let header = TerrainHeader::read_options(&mut cursor, platform.endianness(), ()).ok()?;
-
-        let mut plates = vec![];
-
-        for i in 0..header.plate_count {
-            plates.push(PlateModel {
-                position: (
-                    header.plate_size as f32 * (header.positions[i as usize].x as f32 + 0.5),
-                    header.plate_size as f32 * (header.positions[i as usize].y as f32 + 0.5),
-                ),
-                filename: format!("{:04}.mdl", i),
-            })
-        }
-
-        Some(Terrain { plates })
+        Terrain::read_options(&mut cursor, platform.endianness(), ()).ok()
     }
 }
 
@@ -76,28 +55,25 @@ impl WritableFile for Terrain {
 
         {
             let mut cursor = Cursor::new(&mut buffer);
-
-            let plate_size = 32;
-
-            let header = TerrainHeader {
-                version: 0x1000003,
-                plate_count: self.plates.len() as u32,
-                plate_size,
-                clip_distance: 0.0, // TODO: make configurable
-                unknown: 1.0,       // TODO: what is this
-                positions: self
-                    .plates
-                    .iter()
-                    .map(|model| PlatePosition {
-                        x: ((model.position.0 / plate_size as f32) - 0.5) as i16,
-                        y: ((model.position.1 / plate_size as f32) - 0.5) as i16,
-                    })
-                    .collect(),
-            };
-            header.write_le(&mut cursor).ok()?;
+            self.write_le(&mut cursor).ok()?;
         }
 
         Some(buffer)
+    }
+}
+
+impl Terrain {
+    /// Returns the real position of this plate, taking into account its size.
+    pub fn plate_position(&self, plate: &Plate) -> [f32; 2] {
+        [
+            self.plate_size as f32 * (plate.x as f32 + 0.5),
+            self.plate_size as f32 * (plate.y as f32 + 0.5),
+        ]
+    }
+
+    /// Returns the filename of the `.mdl` for a given plate index.
+    pub fn mdl_filename(plate_index: usize) -> String {
+        format!("{:04}.mdl", plate_index)
     }
 }
 
