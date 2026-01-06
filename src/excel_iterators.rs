887c8be20656ace64a5a7fa7a3805366ae2bd899
Joshua Goins <josh@redstrate.com>
2026-01-05 19:00:39 -0500

    Improve Excel iterator API further
    More importantly, there's a way to flatten subrows in case you don't
    care about them.

--- src/excel_iterators.rs
+++ src/excel_iterators.rs
@@ -3,14 +3,30 @@
 
 use crate::excel::{Page, Row};
 
+/// Iterator over an [Page].
+///
+/// This includes subrows, if you know you don't have those then use [Self::flatten_subrows].
 pub struct ExcelSheetPageIterator<'a> {
     page: &'a Page,
     row_index: u32,
 }
 
+impl<'a> ExcelSheetPageIterator<'a> {
+    /// Flattens this iterator, giving you one that only contains rows.
+    ///
+    /// If this sheet actually has subrows, then it only takes the first one in each row.
+    pub fn flatten_subrows(&self) -> ExcelRowIterator<'a> {
+        ExcelRowIterator {
+            page: self.page,
+            row_index: self.row_index,
+        }
+    }
+}
+
 impl<'a> IntoIterator for &'a Page {
-    type Item = (u32, &'a Row);
+    type Item = (u32, &'a [(u16, Row)]);
     type IntoIter = ExcelSheetPageIterator<'a>;
+
     fn into_iter(self) -> ExcelSheetPageIterator<'a> {
         ExcelSheetPageIterator {
             page: self,
@@ -20,6 +36,31 @@ impl<'a> IntoIterator for &'a Page {
 }
 
 impl<'a> Iterator for ExcelSheetPageIterator<'a> {
+    type Item = (u32, &'a [(u16, Row)]);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let row_index = self.row_index;
+        self.row_index += 1;
+
+        if row_index as usize > self.page.row_count() {
+            return None;
+        }
+
+        let row = &self.page.entries.get(row_index as usize)?;
+
+        Some((row.id, row.subrows.as_slice()))
+    }
+}
+
+/// Iterator over an [Page], but only the rows.
+///
+/// To create this iterator, use [ExcelSheetPageIterator::flatten_subrows].
+pub struct ExcelRowIterator<'a> {
+    page: &'a Page,
+    row_index: u32,
+}
+
+impl<'a> Iterator for ExcelRowIterator<'a> {
     type Item = (u32, &'a Row);
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -32,7 +73,7 @@ impl<'a> Iterator for ExcelSheetPageIterator<'a> {
 
         let row = &self.page.entries.get(row_index as usize)?;
 
-        Some((row.id, &row.subrows[0].1))
+        Some((row.id, &row.subrows.first()?.1))
     }
 }
 
@@ -80,33 +121,83 @@ mod tests {
 
         assert_eq!(excel.pages[0].entries.len(), 4);
 
-        let expected_iterator: Vec<(u32, &Row)> = excel.pages[0].into_iter().collect();
+        // normal behavior that includes subrows
+        let expected_iterator: Vec<(u32, &[(u16, Row)])> = excel.pages[0].into_iter().collect();
         assert_eq!(
             expected_iterator,
             vec![
                 (
-                    1441792,
+                    1441792u32,
+                    [(
+                        0u16,
+                        Row {
+                            columns: vec![Field::Int8(0)]
+                        }
+                    )]
+                    .as_slice()
+                ),
+                (
+                    1441793u32,
+                    [(
+                        0u16,
+                        Row {
+                            columns: vec![Field::Int8(1)]
+                        }
+                    )]
+                    .as_slice()
+                ),
+                (
+                    1441794u32,
+                    [(
+                        0u16,
+                        Row {
+                            columns: vec![Field::Int8(2)]
+                        }
+                    )]
+                    .as_slice()
+                ),
+                (
+                    1441795u32,
+                    [(
+                        0u16,
+                        Row {
+                            columns: vec![Field::Int8(3)]
+                        }
+                    )]
+                    .as_slice()
+                )
+            ]
+        );
+
+        // API for row-only sheets
+        let expected_iterator: Vec<(u32, &Row)> =
+            excel.pages[0].into_iter().flatten_subrows().collect();
+        assert_eq!(
+            expected_iterator,
+            vec![
+                (
+                    1441792u32,
                     &Row {
                         columns: vec![Field::Int8(0)]
-                    }
+                    },
                 ),
                 (
-                    1441793,
+                    1441793u32,
                     &Row {
                         columns: vec![Field::Int8(1)]
-                    }
+                    },
                 ),
                 (
-                    1441794,
+                    1441794u32,
                     &Row {
                         columns: vec![Field::Int8(2)]
-                    }
+                    },
                 ),
                 (
-                    1441795,
+                    1441795u32,
                     &Row {
                         columns: vec![Field::Int8(3)]
-                    }
+                    },
                 )
             ]
         );
