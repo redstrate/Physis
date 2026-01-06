71b8ff7d89ad8a6fd6b9cdf1b53d743d4b374383
Joshua Goins <josh@redstrate.com>
2026-01-06 18:15:45 -0500

    Begin reading TMB files properly

--- src/tmb.rs
+++ src/tmb.rs
@@ -6,26 +6,42 @@ use std::io::Cursor;
 use crate::ByteSpan;
 use crate::ReadableFile;
 use crate::common::Platform;
+use crate::common_file_operations::read_short_identifier;
+use crate::common_file_operations::write_short_identifier;
 use binrw::BinRead;
 use binrw::binrw;
 
 #[binrw]
 #[derive(Debug)]
-struct TmbHeader {
-    magic: i32, // TODO: figure out what this
-    size: i32,
-    entry_count: i32,
+pub struct TimelineNode {
+    #[bw(write_with = write_short_identifier)]
+    #[br(parse_with = read_short_identifier)]
+    tag: String,
+    /// Size in bytes, including the tag.
+    size: u32,
+    #[br(count = size - 8)]
+    data: Vec<u8>,
 }
 
+/// Timeline binary file, usually with the `.tmb` file extension.
+///
+/// Contains animation information, and also seen being embedded into SGBs.
+#[binrw]
+#[brw(magic = b"TMLB")]
 #[derive(Debug)]
-pub struct Tmb {}
+pub struct Tmb {
+    /// In bytes, including this header.
+    file_size: u32,
+    num_nodes: u32,
+    #[br(count = num_nodes)]
+    pub nodes: Vec<TimelineNode>,
+    // NOTE: there is extra data at the end, presumably it contains the actual animation data?,
+}
 
 impl ReadableFile for Tmb {
     fn from_existing(platform: Platform, buffer: ByteSpan) -> Option<Self> {
         let mut cursor = Cursor::new(buffer);
-        TmbHeader::read_options(&mut cursor, platform.endianness(), ()).ok()?;
-
-        Some(Tmb {})
+        Tmb::read_options(&mut cursor, platform.endianness(), ()).ok()
     }
 }
 
