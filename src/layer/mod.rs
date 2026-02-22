d9891fbf1b3198f62286edbb09fd21a7945a1d1b
Joshua Goins <josh@redstrate.com>
2026-02-22 16:47:29 -0500

    Fix LGB for The Orbonne Monastery
    Found another unknown asset type in here!

--- src/layer/mod.rs
+++ src/layer/mod.rs
@@ -163,6 +163,7 @@ pub enum LayerEntryType {
     Unk1 = 90,
     Unk4 = 83, // seen in bg/ex5/01_xkt_x6/twn/x6t1/level/bg.lgb
     Unk2 = 86, // seen in bg/ex5/02_ykt_y6/fld/y6f1/level/bg.lgb
+    Unk5 = 87, // seen in bg/ex2/05_zon_z3/rad/z3r3/level/planmap.lgb
     Unk3 = 89, // seen in bg/ffxiv/sea_s1/fld/s1f3/level/planevent.lgb
 }
 
