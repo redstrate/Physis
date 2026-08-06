8d8fb9e7a9a202e56d4989b0ff25e1fd2b14248e
Joshua Goins <josh@redstrate.com>
2026-08-06 15:26:49 -0400

    Begin extracting useful data from amb files

--- src/amb.rs
+++ src/amb.rs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2026 Joshua Goins <josh@redstrate.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#![allow(clippy::large_enum_variant)] // for now...
+
 use std::io::Cursor;
 
 use crate::ByteBuffer;
@@ -15,37 +17,99 @@ use binrw::BinWrite;
 use binrw::VecArgs;
 use binrw::binrw;
 
+#[binrw]
+#[derive(Debug)]
+pub enum AmbSubtype {
+    #[brw(magic = 0u8)]
+    AmbientWeather(AmbAmbientWeather),
+    #[brw(magic = 1u8)]
+    SkylightSkySetDatabase(AmbSkylightSkySetDatabase),
+}
+
 /// Ambient set file, usually with the `.amb` file extension.
 #[binrw]
 #[derive(Debug)]
 #[brw(magic = b"AMB\0")]
 pub struct Amb {
     /// Should be 1?
-    unk1: u16,
+    pub version: u16,
     /// Should be 0?
-    unk2: u8,
-    /// Should be 0? If it isn't probably an old file or something?
-    unk3: u8,
+    pub endianness: u8,
+    pub data: AmbSubtype,
+}
 
-    #[br(count = 36)] // full of something interesting i'm sure
-    unk4: Vec<u8>,
+#[binrw]
+#[derive(Debug)]
+pub struct AmbAmbientWeather {
+    pub sky_visibility_spherical_harmonics: SecondOrderSphericalHarmonics,
 
     inner_counts: [i32; Self::ENTRY_COUNT],
 
     #[br(parse_with = read_amb_entry, args(&inner_counts,))]
-    pub entries: [Vec<AmbEntry>; Self::ENTRY_COUNT],
+    pub entries: [Vec<AmbWeatherKeyframe>; Self::ENTRY_COUNT],
 }
 
-impl Amb {
+impl AmbAmbientWeather {
     pub(crate) const ENTRY_COUNT: usize = 0x20;
 }
 
+#[binrw]
+#[derive(Debug)]
+pub struct AmbSkylightSkySetDatabase {
+    reserved_08: u16,
+
+    #[br(temp)]
+    #[bw(calc = sky_sets.len() as u16)]
+    sky_set_count: u16,
+    #[br(temp)]
+    #[bw(calc = samples.len() as u32)]
+    total_sample_count: u32,
+
+    #[br(count = sky_set_count)]
+    pub sky_sets: Vec<SkylightSkySetDescriptor>,
+    #[br(count = total_sample_count)]
+    pub samples: Vec<SkylightSphericalHarmonicsSample>,
+}
+
+#[binrw]
+#[derive(Debug)]
+pub struct SecondOrderSphericalHarmonics {
+    pub l0_constant: f32,
+    pub l1_y: f32,
+    pub l1_z: f32,
+    pub l1_x: f32,
+    pub l2_xy: f32,
+    pub l2_yz: f32,
+    pub l2_3z2_minus_1: f32,
+    pub l2_xz: f32,
+    pub l2_x2_minus_y2: f32,
+}
+
+#[binrw]
+#[derive(Debug)]
+pub struct AmbWeatherKeyframe {
+    /// Ambient light spherical harmonics.
+    pub ambient_light: AmbientLightSphericalHarmonics,
+    /// In seconds.
+    pub time_of_day: f32,
+}
+
+#[binrw]
+#[derive(Debug)]
+pub struct AmbientLightSphericalHarmonics {
+    pub red: SecondOrderSphericalHarmonics,
+    pub green: SecondOrderSphericalHarmonics,
+    pub blue: SecondOrderSphericalHarmonics,
+}
+
 #[binrw::parser(reader, endian)]
-fn read_amb_entry(counts: &[i32]) -> BinResult<[Vec<AmbEntry>; Amb::ENTRY_COUNT]> {
-    let mut entries: [Vec<AmbEntry>; Amb::ENTRY_COUNT] = Default::default();
+fn read_amb_entry(
+    counts: &[i32],
+) -> BinResult<[Vec<AmbWeatherKeyframe>; AmbAmbientWeather::ENTRY_COUNT]> {
+    let mut entries: [Vec<AmbWeatherKeyframe>; AmbAmbientWeather::ENTRY_COUNT] = Default::default();
 
     for (i, count) in counts.iter().enumerate() {
-        let entry: Vec<AmbEntry> =
+        let entry: Vec<AmbWeatherKeyframe> =
             reader.read_type_args(endian, VecArgs::builder().count(*count as usize).finalize())?;
         entries[i] = entry;
     }
@@ -53,27 +117,18 @@ fn read_amb_entry(counts: &[i32]) -> BinResult<[Vec<AmbEntry>; Amb::ENTRY_COUNT]
     Ok(entries)
 }
 
-/// Entry into an [Amb] file.
 #[binrw]
 #[derive(Debug)]
-pub struct AmbEntry {
-    unk1: f32,
-    unk2: f32,
-    unk3: f32,
-    unk4: f32,
+pub struct SkylightSkySetDescriptor {
+    pub sky_set_id: u16,
+    pub sample_count: u16,
+    pub first_sample_index: u32,
+}
 
-    unk5: u64,
-    unk6: u64,
-    unk7: u64,
-    unk8: u64,
-    unk9: u64,
-    unk10: u64,
-    unk11: u64,
-    unk12: u64,
-    unk13: u64,
-    unk14: u64,
-    unk15: u64,
-    unk16: u64,
+#[binrw]
+#[derive(Debug)]
+pub struct SkylightSphericalHarmonicsSample {
+    pub ambient_light: AmbientLightSphericalHarmonics,
 }
 
 impl ReadableFile for Amb {
